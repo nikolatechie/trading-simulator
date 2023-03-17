e541970ab199b16a93c1b7446235ca8555e2867e
nikolatechie <nikola-grujic@hotmail.com>
2023-03-17 18:10:58 +0100

    Initial backend setup

---
+++ backend/src/main/java/com/nikolagrujic/tradingsimulator/TradingSimulatorBackendApplication.java
@@ -0,0 +1,11 @@
+package com.nikolagrujic.tradingsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSimulatorBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TradingSimulatorBackendApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/nikolagrujic/tradingsimulator/TradingSimulatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolagrujic.tradingsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingSimulatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
